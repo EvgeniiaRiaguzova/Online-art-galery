482852a602636d989cbd3e3901168af10253aba4
Evgeniia Riaguzova <aglaya_2004@mail.ru>
2022-06-29 11:57:48 +0200

    installed ironlauncher, created signup view and route, validation, user-profile view

---
+++ online-art-galery/models/User.model.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the user model to whatever makes sense in this case
+const userSchema = new Schema({
+  username: {
+    type: String,
+    trim: true,
+    required: [true, 'Username is required.'],
+    unique: true
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required.'],
+    // this match will disqualify all the emails with accidental empty spaces, missing dots in front of (.)com and the ones with no domain at all
+    match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address.'],
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  status: 
+  {
+   type: String,
+   required: [true, 'Status is required.']
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required.']
+  }
+});
+
+const User = model("User", userSchema);
+
+module.exports = User;
