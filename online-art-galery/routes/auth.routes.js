e9d5bcdc304192c8774d777bf2d3e3c01e4a936e
Evgeniia Riaguzova <aglaya_2004@mail.ru>
2022-06-30 16:46:29 +0200

    login, walidation, signup

--- online-art-galery/routes/auth.routes.js
+++ online-art-galery/routes/auth.routes.js
@@ -5,6 +5,7 @@ const saltRounds = 10;
 const User = require('../models/User.model');
 const mongoose = require('mongoose');
 
+//////////// S I G N U P ///////////
 // GET route ==> to display the signup form to users
 router.get('/signup', (req, res) => res.render('auth/signup'));
 // POST route ==> to process form data
@@ -53,5 +54,36 @@ router.post('/signup', (req, res, next) => {
             }
           }) // close .catch()
       }) 
+
+//////////// L O G I N ///////////
+ 
+// GET route ==> to display the login form to users
+router.get('/login', (req, res) => res.render('auth/login'));
+
+// POST login route ==> to process form data
+router.post('/login', (req, res, next) => {
+    const { email, password } = req.body;
+   
+    if (email === '' || password === '') {
+      res.render('auth/login', {
+        errorMessage: 'Please enter both, email and password to login.'
+      });
+      return;
+    }
+   
+    User.findOne({ email })
+      .then(user => {
+        if (!user) {
+          res.render('auth/login', { errorMessage: 'Email is not registered. Try with other email.' });
+          return;
+        } else if (bcryptjs.compareSync(password, user.passwordHash)) {
+          res.render('users/user-profile', { user });
+        } else {
+          res.render('auth/login', { errorMessage: 'Incorrect password.' });
+        }
+      })
+      .catch(error => next(error));
+  });
+  
     router.get('/userProfile', (req, res) => res.render('users/user-profile')); 
 module.exports = router;
